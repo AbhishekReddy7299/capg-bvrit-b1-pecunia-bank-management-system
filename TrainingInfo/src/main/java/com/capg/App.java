e1b7555188e3ce929f47da4e2b6e9ce9df2440e4
SunnY <SunnY@SunnY>
2020-04-11 15:23:14 +0530

    2 program

---
+++ TrainingInfo/src/main/java/com/capg/App.java
@@ -0,0 +1,20 @@
+package com.capg;
+
+ 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+ 
+public class App {
+	 public static void main( String[] args )
+	    {
+	       
+		 ApplicationContext context=
+	    		   new AnnotationConfigApplicationContext(Configurations.class);
+	       Trainee t=(Trainee) context.getBean("trainee");
+
+	       t.display();
+
+	    	
+	    }
+}
