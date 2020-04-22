421a22c775ba047d8d11f9bd1f7e4e4b98b9e553
SunnY <SunnY@192.168.1.7>
2020-04-22 14:59:46 +0530

    modules

---
+++ capg-bvrit-b1-pecunia-bank-management/src/main/java/com/capg/pbms/CapgBvritB1PecuniaBankManagementApplication.java
@@ -0,0 +1,13 @@
+package com.capg.pbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgBvritB1PecuniaBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgBvritB1PecuniaBankManagementApplication.class, args);
+	}
+
+}
---
+++ capg-bvrit-b1-pecunia-bank-management/src/test/java/com/capg/pbms/CapgBvritB1PecuniaBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.pbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgBvritB1PecuniaBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
