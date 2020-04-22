77c4bda1ed42a418b88480f593a0b07a299d1da5
prameela <prameela5128@gmail.com>
2020-04-22 17:05:11 +0530

    commit

---
+++ pbms-transaction-using-cheque/src/main/java/com/capg/pbms/transactionUsingCheque/PbmsTransactionUsingChequeApplication.java
@@ -0,0 +1,13 @@
+package com.capg.pbms.transactionUsingCheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbmsTransactionUsingChequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbmsTransactionUsingChequeApplication.class, args);
+	}
+
+}
---
+++ pbms-transaction-using-cheque/src/main/java/com/capg/pbms/transactionUsingCheque/model/Address.java
@@ -0,0 +1,84 @@
+package com.capg.pbms.transactionUsingCheque.model;
+
+public class Address {
+	private String addressId;
+	private String addressLine1;
+	private String addressLine2;
+	private String addressCity;
+	private String addressState;
+	private String addressCountry;
+	private String addressZipcode;
+	
+	public Address() {
+		
+	}
+
+	public Address(String addressId, String addressLine1, String addressLine2, String addressCity, String addressState,
+			String addressCountry, String addressZipcode) {
+		super();
+		this.addressId = addressId;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.addressCity = addressCity;
+		this.addressState = addressState;
+		this.addressCountry = addressCountry;
+		this.addressZipcode = addressZipcode;
+	}
+
+	public String getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getAddressCity() {
+		return addressCity;
+	}
+
+	public void setAddressCity(String addressCity) {
+		this.addressCity = addressCity;
+	}
+
+	public String getAddressState() {
+		return addressState;
+	}
+
+	public void setAddressState(String addressState) {
+		this.addressState = addressState;
+	}
+
+	public String getAddressCountry() {
+		return addressCountry;
+	}
+
+	public void setAddressCountry(String addressCountry) {
+		this.addressCountry = addressCountry;
+	}
+
+	public String getAddressZipcode() {
+		return addressZipcode;
+	}
+
+	public void setAddressZipcode(String addressZipcode) {
+		this.addressZipcode = addressZipcode;
+	}
+	
+}
