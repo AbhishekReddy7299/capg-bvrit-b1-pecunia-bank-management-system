2c359c3dea7f9cb65aaf34d69646cd23d7fbf348
prameela <prameela5128@gmail.com>
2020-04-23 18:45:16 +0530

    commit

--- pbms-transaction-using-cheque/src/test/java/com/capg/pbms/transactionusingcheque/PbmsTransactionUsingChequeApplicationTests.java
+++ pbms-transaction-using-cheque/src/test/java/com/capg/pbms/transactionusingcheque/PbmsTransactionUsingChequeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.capg.pbms.transactionUsingCheque;
+package com.capg.pbms.transactionusingcheque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
