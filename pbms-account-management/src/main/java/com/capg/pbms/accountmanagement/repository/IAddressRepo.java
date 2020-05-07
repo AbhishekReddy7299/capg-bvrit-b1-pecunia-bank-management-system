4a86e4fa7a1ebc66aeedc34bd6cfa653af93484e
Abhishek Reddy Balannolla <52633368+AbhishekReddy7299@users.noreply.github.com>
2020-05-07 10:36:00 +0530

    Delete IAddressRepo.java

--- pbms-account-management/src/main/java/com/capg/pbms/accountmanagement/repository/IAddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capg.pbms.accountmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.pbms.accountmanagement.model.Address;
-
-public interface IAddressRepo extends JpaRepository<Address, Long> {
-
-}
