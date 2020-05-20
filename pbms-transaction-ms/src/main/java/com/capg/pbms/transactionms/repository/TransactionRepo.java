50ef8f02e738e20f5cf6b387d3464e4e11fb0c5a
prameela <prameela5128@gmail.com>
2020-05-20 12:25:14 +0530

    Delete TransactionRepo.java

--- pbms-transaction-ms/src/main/java/com/capg/pbms/transactionms/repository/TransactionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capg.pbms.transactionms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.pbms.transactionms.model.Transaction;
-
-public interface TransactionRepo extends JpaRepository<Transaction,String> {
-
-}
