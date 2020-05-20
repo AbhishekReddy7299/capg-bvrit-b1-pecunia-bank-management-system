dccaf3299c483b5a125331f3c4a2cb35fe7d9969
prameela <prameela5128@gmail.com>
2020-05-20 12:25:22 +0530

    Delete ChequeRepo.java

--- pbms-transaction-ms/src/main/java/com/capg/pbms/transactionms/repository/ChequeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capg.pbms.transactionms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.pbms.transactionms.model.Cheque;
-
-public interface ChequeRepo extends JpaRepository<Cheque,String>{
-
-}
